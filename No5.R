b8ac3726edfc80930e3716a0a643ae36e006e1de
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2022-04-10 17:57:24 +0700

    Add files via upload

---
+++ No5.R
@@ -0,0 +1,15 @@
+x = 3
+
+# No 5a
+dexp(x, rate = 1, log = FALSE)
+
+# No 5b
+set.seed(1)
+par(mfrow=c(2,2))
+set.seed(1)
+hist(rexp(10))
+hist(rexp(100))
+hist(rexp(1000))
+hist(rexp(10000))
+
+# No 5c

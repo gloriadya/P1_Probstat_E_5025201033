a6f9242596a6fd2d3bc758c8e8b672fa20c9f15a
Gloria Dyah Pramesti <91613088+gloriadya@users.noreply.github.com>
2022-04-10 18:12:47 +0700

    Add files via upload

---
+++ No6.R
@@ -0,0 +1,12 @@
+n = 100
+mean = 50
+sd = 8
+
+# No 6a
+
+# No 6b
+hist(rnorm(n = 100, mean = 50, sd = 8), breaks = 50, main = "5025201033_Gloria Dyah Pramesti_Probstat_E_DNhistogram")
+
+# No 6c
+var = sd * sd
+var
